f68a87ce3b8313d88f5af60efe333c4750c8049f
Gabriel Jony <gabjony@gmail.com>
2020-04-19 20:50:40 -0300

    Componente Atualizar Produto #02

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/model/product.model';
 
 @Component({
   selector: 'app-product-update',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
+  
+  product: Product;
 
-  constructor() { }
+  constructor(private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // pega o parametro que é passado pela url o id
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.productService.readById(id).subscribe((product) => {
+      this.product = product;
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage("Produto atualizado com sucesso!");
+      this.router.navigate(["/product"]);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(["/product"]);
   }
 
 }
