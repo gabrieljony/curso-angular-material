811ade260d6b59b0741668c3cbbeb0d8714ff0ba
Gabriel Jony <gabjony@gmail.com>
2020-04-23 11:29:17 -0300

    Separanod os module do material da module do app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,38 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID} from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from  '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import { MatCardModule } from '@angular/material/card';
 import { ProductComponent } from './views/product/product.component';
 import { BlueDirective } from './directives/blue.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { HttpClientModule } from '@angular/common/http';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { MatTableModule } from '@angular/material/table';
 
-import localePt from '@angular/common/locales/pt';
-import { registerLocaleData } from  '@angular/common';
 import { TableComponent } from './components/schematics/table/table.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { MaterialModule } from './material.module';
+
 registerLocaleData(localePt);
 
 @NgModule({
@@ -57,17 +47,7 @@ registerLocaleData(localePt);
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatCardModule,
-    MatButtonModule,
-    MatSnackBarModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule
+    MaterialModule
   ],
   providers: [{
     provide: LOCALE_ID,
---
+++ frontend/src/app/material.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+
+@NgModule({
+    imports: [
+      MatToolbarModule,
+      MatSidenavModule,
+      MatListModule,
+      MatCardModule,
+      MatButtonModule,
+      MatSnackBarModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatSortModule
+      ],
+    exports: [
+      MatToolbarModule,
+      MatSidenavModule,
+      MatListModule,
+      MatCardModule,
+      MatButtonModule,
+      MatSnackBarModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatSortModule
+      ]
+  })
+  export class MaterialModule { }
